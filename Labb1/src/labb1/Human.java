f75fd049b0904a5e2934ab2b106282b8f3fd202a
oornberg <oornberg@4a70a31b-f663-c608-99c9-fb883f79e3b9>
2017-11-07 10:43:27 +0000

    Började på del 3.

--- Labb1/src/labb1/Human.java
+++ Labb1/src/labb1/Human.java
@@ -19,18 +19,49 @@ public class Human {
         System.out.println(newHuman.getAge());
         System.out.println(newHuman.getName());
         System.out.println(newHuman);
+        Human fysiker=new Human();
+        System.out.println(fysiker);
     }    
     private int age;
-    private String name;
+    private String name; 
+    private String[] fysikerName;
+    
     public void  setAge(int a){
     age=a;
     }
     public void setName(String n){
         name=n; 
     }
+    
+    public void setFysikerName(){
+        String[] fysikerName;
+        fysikerName=new String[15];
+        fysikerName[0]="Oscar";
+        fysikerName[1]="Martin";
+        fysikerName[2]="André";
+        fysikerName[3]="Emil";
+        fysikerName[4]="Anders";
+        fysikerName[5]="Klas";
+        fysikerName[6]="Thomas";
+        fysikerName[7]="Max";
+        fysikerName[8]="Ebba";
+        fysikerName[9]="Anton";
+        fysikerName[10]="Linn";
+        fysikerName[11]="Tobbe";
+        fysikerName[12]="Leo";
+        fysikerName[13]="Marcus";
+        fysikerName[14]="Ullis";
+    }
+    
     public Human(int ageln, String nameln){
     age=ageln;
     name=nameln;        
+    }
+    
+    public Human(){
+        int randAge=(int)(Math.random()*100);
+        String randName=fysikerName[(int)(Math.random()*15)];
+        Human(randAge, randName);
     }
     public String toString(){
      return "namn: " + name + ", ålder: " + age + " år.";
@@ -44,6 +75,7 @@ public class Human {
     public int getAge(){
         return age;
     }
+
 }
 
 
